2bdd7ecc1ec5a70646fa7d907761f12b3cdc3090
bilal001 <56475491+Ambush64@users.noreply.github.com>
2022-08-30 17:22:08 +0530

    second

--- src/app.js
+++ src/app.js
@@ -87,6 +87,19 @@ app.get("/tournament", (req, res) => {
 // //   });
 // });
 
+// heroku
+
+// heroku
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
